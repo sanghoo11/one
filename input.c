81a179be88a7b08987b85c1f00687cf96be30424
sanghoo11 <sh1104yoon@gmail.com>
2026-06-13 15:47:36 +0900

    I want to go home

---
+++ input.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	/*int result;
+	int num1, num2, num3;
+	printf("세 개의 정수 입력: ");
+	scanf("%d %d %d", &num1, &num2, &num3);
+	result = num1 + num2 + num3;
+	printf(" %d + %d + %d = %d \n", num1, num2, num3, result);
+	int result;
+	int result2;
+	int num1, num2;
+	printf("두 개의 정수 입력:");
+	scanf("%d %d ", &num1, &num2);
+	result = num1 + num2;
+	result2 = num1 - num2;
+	printf("%d", result);
+int result;
+int num1, num2, num3;
+printf("세 개의 정수 입력: ");
+scanf("%d %d %d", &num1, &num2, &num3);
+result = num1 * num2 + num3;
+printf(" %d X %d + %d = %d \n", num1, num2, num3, result);
+	int result;
+	int num1;
+	printf(한 개의 정수 입력 ");
+	scanf("%d", &num1);
+	result = num1 * num1;
+	printf("%d의 제곱은 %d \n", num1, result);
+	int result,result2;
+	int num1,num2;
+	printf("두 개의 정수 입력 ");
+	scanf("%d %d", &num1, &num2);
+	result = num1 / num2;
+	result2 = num1 % num2;
+	printf(" %d 나누기 몫 %d = %d %d 나누기 나머지 %d = %d \n", num1, num2, result, num1, num2, result2);
+	int result, result2, result3,result4;
+	int num1, num2, num3;
+	printf("세 개의 정수 입력 ");
+	scanf("%d %d %d", &num1, &num2, &num3);
+	result = num1 - num2;
+	result2 = num2 + num3;
+	result3 = num3 % num1;
+	result4 = result * result2 * result3;
+	printf("(num1-num2)X(num2+num3)X(num3%num1) = %d \n", num1, num2, num2, num3, num3, num1, result4);*/
+	int num1;
+	printf("한 개의 정수 입력 ");
+	scanf("%d",&num1);
+	printf("%d X %d = %d\n", num1, 1, num1 * 1);
+	printf("%d X %d = %d\n", num1, 2, num1 * 2);
+	printf("%d X %d = %d\n", num1, 3, num1 * 3);
+	printf("%d X %d = %d\n", num1, 4, num1 * 4);
+	printf("%d X %d = %d\n", num1, 5, num1 * 5);
+	printf("%d X %d = %d\n", num1, 6, num1 * 6);
+	printf("%d X %d = %d\n", num1, 7, num1 * 7);
+	printf("%d X %d = %d\n", num1, 8, num1 * 8);
+	printf("%d X %d = %d\n", num1, 9, num1 * 9);
+
+	return 0;
+}
